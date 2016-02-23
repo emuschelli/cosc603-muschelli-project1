531288225d1f2d64093a04346356ea267da620ed
emuschelli <emuschelli@yahoo.com>
2016-02-22 22:41:23 -0500

    Added files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @author Erica Muschelli
+ * @version 1.0
+ * @since 22 FEB 2016
+ * 
+ * Prints out the message "Hello World" to the Console.
+ * 
+ * 
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+
+	}
+
+}
