040d9461151d1e7edd87ede1699631de4009deda
emuschelli <emuschelli@yahoo.com>
2016-02-22 22:59:55 -0500

    Removed Comments from Code
    Removed comments from HelloWorld testing file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,4 @@
-/**
- * 
- * @author Erica Muschelli
- * @version 1.0
- * @since 22 FEB 2016
- * 
- * Prints out the message "Hello World" to the Console.
- * 
- * 
- */
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
