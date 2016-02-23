f659109f158524c90ef14d39a3dd7aca499c6fa0
emuschelli <emuschelli@yahoo.com>
2016-02-22 23:50:25 -0500

    Begin testing GitHub Desktop Functionality with FireDangerMain file.

---
+++ Project1/src/FireDangerMain.java
@@ -0,0 +1,23 @@
+/**
+ * Calculation of National Fire Danger Rating Indexes used by the North Central Forest Experiment Station, Forest Services
+ * <P>
+ * U.S. Department of Agriculture
+ */
+
+/**
+ * @author Erica L. Muschelli
+ * @version 1.0
+ * @since 22 FEB 2016
+ * 
+ */
+public class FireDangerMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	System.out.println("I am here...");
+
+	}
+
+}
